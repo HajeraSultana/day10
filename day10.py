566c1af33777f4dcaa6ffc787427d5dba4f64071
HajeraSultana <e031306@gmail.com>
2024-09-05 07:14:11 +0100

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,10 @@
+print ("***Tip Calculatir***")
+billamount = float(input("What was the total bill? "))
+tip = int(input("How much would you like to tip? "))
+people = int(input("How many people to split the bill? "))
+tip_percentage = tip/100
+total_tip = billamount * tip_percentage
+total_bill = billamount + total_tip
+finalamount = total_bill/people
+eachshare = round(finalamount,2)
+print("each person should pay",eachshare)
